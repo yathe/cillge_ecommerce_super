d16278354555ba8af7bc935684b33fc3826f3177
md shahid iqbal <111365870+iqbalshaid@users.noreply.github.com>
2025-08-27 16:19:25 +0530

    Update route.js

--- app/api/coupon/newcoupon/route.js
+++ app/api/coupon/newcoupon/route.js
@@ -14,7 +14,11 @@ export async function POST(request) {
     if (existingCoupon) {
       return NextResponse.json({ success: false, message: "Coupon code already exists" });
     }
-    
+    //only one user create only one time coupon
+    const checkcoupon = await Coupon.findOne({ownerUserId:userId});
+    if (checkcoupon) {
+      return NextResponse.json({ success: false, message: "You before generated coupon,then you not generate" });
+    }
     const newCoupon = await Coupon.create({
       code,
       ownerUserId: userId, // This is now a string
